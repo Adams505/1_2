1fd5e54436fa5f564690e289100444e75d9e09e8
Adams505 <101186913+Adams505@users.noreply.github.com>
2022-03-22 11:37:10 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,13 @@
+class Main {
+  public static void main(String[] args) {
+    int a =5;
+    double b=5.5;
+    char c='j';
+    long p = 192939292;
+    System.out.println(a);
+    System.out.println(b);
+    System.out.println(c);
+    System.out.println(p);
+   
+  }
+}
